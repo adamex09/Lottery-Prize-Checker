8305e699d3b108d776bcd7380e17007ac437edf8
Adam Hornyak <hello@adamhornyak.com>
2017-05-01 01:20:17 +0200

    scrape

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express()
-var matches = [];
+var prize_amount = [];
 var regex = /[\d|,|.|e|E|\+]+/g;
 
 app.set('port', (process.env.PORT || 5000))
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
 })
 
 
-request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok", function(error, response, body) {
+request("https://bet.szerencsejatek.hu/jatekok/putto", function(error, response, body) {
   if(error) {
     console.log("Error: " + error);
   }
@@ -25,9 +25,15 @@ request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok", function(e
   $('div.grid.game-details.top-banner-text').each(function( index ) {
     var prize = $(this).find('div.expected-price > h3').text().trim();
     console.log("Prize: " + prize);
-    matches = prize.match(regex);
+    prize_amount = prize.match(regex);
   });
-  console.log("Prize (clean): " + matches);
+  prize_amount = prize_amount.replace( /,/g, "" );
+  var prize_amount = parseInt("prize_amount");
+  console.log("Prize amount: " + prize_amount);
+  if (prize_amount>1000) {
+    console.log("Prize is bigger than 1 billion");
+  }
+
 });
 
 
