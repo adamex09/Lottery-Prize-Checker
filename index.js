dcec683d20ab922cc48956e6927a4905389c1f75
Adam Hornyak <hello@adamhornyak.com>
2017-05-01 01:01:52 +0200

    scrape

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require('express')
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express()
-var matches;
-var regex;
+var matches = [];
+var regex = /[\d|,|.|e|E|\+]+/g;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -25,7 +25,6 @@ request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok", function(e
   $('div.grid').each(function( index ) {
     var prize = $(this).find('div.expected-price > h3').text().trim();
     console.log("Prize: " + prize);
-    regex = /[\d|,|.|e|E|\+]+/g;
     matches = prize.match(regex);
   });
   console.log("Prize (clean): " + matches);
