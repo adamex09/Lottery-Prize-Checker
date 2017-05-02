5b02e1b984bdded1c01d0abad0cee392364fc398
Adam Hornyak <hello@adamhornyak.com>
2017-05-02 21:58:24 +0200

    html page

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var cheerio = require('cheerio');
 var app = express()
 var prize = "";
 
+app.set('view engine', 'pug')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
 
 request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok/", function(error, response, body) {
   if(error) {
@@ -26,13 +26,12 @@ request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok/", function(
   } 
   else {
     console.log("Prize is smaller than 1 billion");
-    app.get('/', function(request, response) {
-      response.send("Várható Ötöslottó főnyeremény: " + prize + ", ez még nem éri meg! :)")
+    app.get('/', function (req, res) {
+      res.render('index', { title: 'Hey', message: 'Hello there!' })
     })
   }
 
 });
-
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
