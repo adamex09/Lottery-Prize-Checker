433d68221ba8811ee46cdf871077c1763156aed8
Adam Hornyak <hello@adamhornyak.com>
2017-05-05 22:47:41 +0200

    scheduler

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-//Változók és dependenciák
+//Variables and dependencies
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var sendmail = require('sendmail')();
+var schedule = require('node-schedule');
 var app = express();
 var prize5 = "";
 var prize6 = "";
-//App inicializálás
+
+//App start
 app.set('view engine', 'pug')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-//Ötöslottó scrape
+
+//Lotto 5 scrape
 request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok/", function(error, response, body) {
   if(error) {
     console.log("Error: " + error);
@@ -21,7 +24,8 @@ request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok/", function(
     console.log("Prize5: " + prize5);
   });
 });
-//Hatoslottó scrape
+
+//Lotto 6 scrape
 request("https://bet.szerencsejatek.hu/jatekok/hatoslotto/sorsolasok/", function(error, response, body) {
   if(error) {
     console.log("Error: " + error);
@@ -32,41 +36,66 @@ request("https://bet.szerencsejatek.hu/jatekok/hatoslotto/sorsolasok/", function
     console.log("Prize6: " + prize6);
   });
 });
-//Ellenőrzés
+
+//Prize checker
 function check() {
   if (prize5.includes('milliárd')){
     console.log("Prize5 is bigger than 1 billion");
+    sendmail({
+      from: 'Lottónyeremény Ellenőr <lottery-prize-checker@herokuapp.com>',
+      to: 'hello@adamhornyak.com',
+      subject: 'Játszani kell!',
+      text: 'Az Ötöslottó főnyereménye már 1 milliárd forint felett jár.',
+    });
     app.get('/', function (req, res) {
       res.render('index', { message: 'Játszani kell!', subline: 'Az Ötöslottó eheti várható főnyereménye már ' + prize5 + ', de a Hatoslottóé még csak ' + prize6 + '.' })
     })
   }
   if (prize6.includes('milliárd')){
     console.log("Prize6 is bigger than 1 billion");
+    sendmail({
+      from: 'Lottónyeremény Ellenőr <lottery-prize-checker@herokuapp.com>',
+      to: 'hello@adamhornyak.com',
+      subject: 'Játszani kell!',
+      text: 'A Hatoslottó főnyereménye már 1 milliárd forint felett jár.',
+    });
     app.get('/', function (req, res) {
       res.render('index', { message: 'Játszani kell!', subline: 'Az Hatoslottó eheti várható főnyereménye már ' + prize6 + ', de az Ötöslottóé még csak ' + prize5 + '.' })
     })
   }
   if (prize5.includes('milliárd') && prize6.includes('milliárd')){
     console.log("Prize5 and Prize6 are bigger than 1 billion");
+    sendmail({
+      from: 'Lottónyeremény Ellenőr <lottery-prize-checker@herokuapp.com>',
+      to: 'hello@adamhornyak.com',
+      subject: 'Duplán megéri!',
+      text: 'Az Ötöslottó és a Hatoslottó főnyereménye is 1 milliárd forint felett jár már.',
+    });
     app.get('/', function (req, res) {
-      res.render('index', { message: 'Duplán játszani kell!', subline: 'Az Ötöslottó eheti várható főnyereménye már ' + prize5 + ', és a Hatoslottóé is ' + prize6 + '.' })
+      res.render('index', { message: 'Duplán megéri!', subline: 'Az Ötöslottó eheti várható főnyereménye már ' + prize5 + ', és a Hatoslottóé is ' + prize6 + '.' })
     })
   } 
   else {
     console.log("Prizes are smaller than 1 billion");
-    sendmail({
-      from: 'Lottónyeremény Ellenőr <lottery-prize-checker@herokuapp.com>',
-      to: 'hello@adamhornyak.com',
-      subject: 'Ejj, ráérünk arra még!',
-      text: 'Az Ötöslottó eheti várható főnyereménye még csak ' + prize5 + ', a Hatoslottóé pedig ' + prize6 + '.',
-    });
     app.get('/', function (req, res) {
       res.render('index', { message: 'Ejj, ráérünk arra még!', subline: 'Az Ötöslottó eheti várható főnyereménye még csak ' + prize5 + ', a Hatoslottóé pedig ' + prize6 + '.' })
     })
   }
 }
 check(prize5, prize6)
-//Port figyelés
+
+//Email scheduler
+var j = schedule.scheduleJob({hour: 20, minute: 55, dayOfWeek: 5}, function(){
+  console.log('Time for tea!');
+});
+
+//Port listening
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+//Keep alive
+var http = require("http");
+setInterval(function() {
+    http.get("http://lottery-prize-checker.herokuapp.com");
+}, 3000000);
