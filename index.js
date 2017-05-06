7c1aba72d25b4752e0ca097d6b64c7ae241b8b3e
Adam Hornyak <hello@adamhornyak.com>
2017-05-06 23:02:56 +0200

    html check

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function check() {
   } 
   else {
     console.log("Prizes are smaller than 1 billion");
-    console.log(prize5.includes('milli'));
+    console.log('milliárd'.includes('milli'));
     app.get('/', function (req, res) {
       res.render('index', { icon: 'hourglass_empty', message: 'Ejj, ráérünk arra még!', subline: 'Az Ötöslottó eheti várható főnyereménye még csak ' + prize5 + ', a Hatoslottóé pedig ' + prize6 + '.' })
     })
