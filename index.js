8dc3bffd4fdd99b13cb88f6b8be8e97ee51b5c21
Adam Hornyak <hello@adamhornyak.com>
2017-05-09 21:19:49 +0200

    create database

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ var request = require('request');
 var cheerio = require('cheerio');
 var sendmail = require('sendmail')();
 var schedule = require('node-schedule');
+var pg = require('pg');
 var app = express();
 var prize5 = '';
 var prize6 = '';
+
+//Database config
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 //App start
 app.set('view engine', 'pug')
