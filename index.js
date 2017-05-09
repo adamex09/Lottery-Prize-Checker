aa96ecb8782a339c77a8ad0841150b6837447f7d
Adam Hornyak <hello@adamhornyak.com>
2017-05-09 23:58:35 +0200

    db output

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
   client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .query('SELECT * FROM users;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
     });
+    app.get('/db', function (req, res) {
+      res.send(row)
+    })
 });
 
 //App start
