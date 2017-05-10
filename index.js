032c173fec4f0d061f72f0bcce47954697287352
Adam Hornyak <hello@adamhornyak.com>
2017-05-11 00:08:35 +0200

    prize number formatting

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var app = express();
 var prize5 = '';
 var prize6 = '';
 var date = new Date();
-var NUMERIC_REGEXP = ;
 
 //Database config
 pg.defaults.ssl = true;
@@ -39,10 +38,12 @@ request("https://bet.szerencsejatek.hu/jatekok/otoslotto/sorsolasok/", function(
     console.log("Prize5-raw: " + prize5)
     if (prize5.includes('millió')) {
       prize5 = prize5.match(/[-]{0,1}[\d.]*[\d]+/g)
+      prize5 = prize5.substring(0, prize5.indexOf(','));
       console.log("Prize5: " + prize5)
     }
     else if (prize5.includes('milliárd')) {
       prize5 = prize5.match(/[-]{0,1}[\d.]*[\d]+/g)
+      prize5 = prize5.substring(0, prize5.indexOf(','));
       console.log("Prize5: " + prize5)
     }
   });
