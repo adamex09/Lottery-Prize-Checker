214ff455c200276343294ca4edfa9d10ab73a969
Adam Hornyak <hello@adamhornyak.com>
2017-05-16 22:26:49 +0200

    scheduler

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ var j = schedule.scheduleJob({hour: 10, minute: 0, dayOfWeek: 1}, function(){
 });
 
 var rule = new schedule.RecurrenceRule();
-rule.minute = 11;
+rule.minute = 29;
 
 var j = schedule.scheduleJob(rule, function(){
   var hour = date.getHours();
